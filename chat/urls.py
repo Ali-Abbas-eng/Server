ec0eb7bbfa3c8c89fc77078440f06b50b320875a
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-02-15 12:54:25 +0300

    [ESP-12] first phase real-time chat

--- chat/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('echo-chat-test/', views.lobby)
-]
--- src/urls.py
+++ src/urls.py
@@ -17,11 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path('super-users-axxes/', admin.site.urls),
     path('api-auth/', include("rest_framework.urls")),
     path('api/v1/dj-rest-auth/', include("dj_rest_auth.urls")),
     path('api/v1/dj-rest-auth/registration/', include("dj_rest_auth.registration.urls")),
-    path('', include('chat.urls')),
+    path('', include('speaking_session.urls')),
 ]
