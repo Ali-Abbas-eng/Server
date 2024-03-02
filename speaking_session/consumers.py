1b9908e863b710963d1d54e0cef98dfee4636e6b
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-03-02 10:55:48 +0300

    [ESP-31] handled the case on no voice input (initialising conversation)

--- speaking_session/consumers.py
+++ speaking_session/consumers.py
@@ -9,7 +9,6 @@ import os
 class SpeakingSessionConsumer(AsyncWebsocketConsumer):
     def __init__(self):
         super().__init__()
-        # self.ai_processor = ai_request_handlers.BaseConversationalist(settings.MEDIA_ROOT)
         self.ai_processor = SpeakingSessionManager(GeneralConversationalist())
         user_id = '0'  # later replace with user's actual id
         self.path_template = os.path.join(settings.BASE_DIR, settings.MEDIA_ROOT, 'audios', user_id)
@@ -21,12 +20,14 @@ class SpeakingSessionConsumer(AsyncWebsocketConsumer):
                                               "message": "You are now connected"}))
 
     async def receive(self, text_data=None, bytes_data=None):
+        input_audio_file_path = None
+        output_audio_file_path = os.path.join(self.path_template, '0_out')
         if bytes_data:
             input_audio_file_path = os.path.join(self.path_template, '0_in.wav')
-            output_audio_file_path = os.path.join(self.path_template, '0_out')
             with open(input_audio_file_path, 'wb') as audio_file:
                 audio_file.write(bytes_data)
                 audio_file.close()
-            response = self.ai_processor(input_audio_file_path, output_audio_file_path)
-            response['audio'] = response['audio'].replace(settings.MEDIA_ROOT, "http://127.0.0.1:8000/media")
-            await self.send(text_data=json.dumps(response))
+
+        response = self.ai_processor(input_audio_file_path, output_audio_file_path)
+        response['audio'] = response['audio'].replace(settings.MEDIA_ROOT, "http://127.0.0.1:8000/media")
+        await self.send(text_data=json.dumps(response))
