eac5d91e7ee72df81731e4543c8e3a480003d8b1
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-03-08 07:08:58 +0300

    [ESP-41] image and backround are now fields of the database, image url is generated upon request

--- chat_scenarios/models.py
+++ chat_scenarios/models.py
@@ -1,5 +1,6 @@
+# chat_scenario/models.py
 from django.db import models
-
+from django.conf import settings
 
 class ChatScenario(models.Model):
     id = models.AutoField(primary_key=True)
@@ -7,6 +8,22 @@ class ChatScenario(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(default="")
     number_of_messages = models.IntegerField(default=15)
+    background = models.ImageField(upload_to='chat_scenarios/background', null=True, blank=True)
+    image = models.ImageField(upload_to='chat_scenarios/image', null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def background_url(self):
+        if self.background and hasattr(self.background, 'url'):
+            return self.background.url
+        else:
+            return ""
+
+    @property
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
+        else:
+            return ""
