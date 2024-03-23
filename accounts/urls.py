a114277bcb9f2471fb825f09463724e1199fffe2
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-03-23 09:47:14 +0300

    [ESP-39] build authentication pipelines

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 from .views import LoginView, RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('api/v1/login/', LoginView.as_view(), name='account_login'),
-    path('api/v1/register/', RegisterView.as_view(), name='account_register')
+    path('api/v1/register/', RegisterView.as_view(), name='account_register'),
+    path('api/v1/token/obtain/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- chat_scenarios/urls.py
+++ chat_scenarios/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('chat_scenarios/', views.chat_scenario_list, name='chat_scenario_list'),
+    path('chat_scenarios/', views.ChatScenarioListView.as_view(), name='chat_scenario_list'),
 ]
